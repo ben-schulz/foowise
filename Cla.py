40cae213366ce6314aa53434b4f65a92b897a989
ben-schulz <benjamin.john.schulz@gmail.com>
2018-04-26 15:25:12 -0500

    feat(classification): implement validity relation. Change internal colleciton types in 'Cla' to set. On 'addValidity', update 'tok' and 'typ' properties for consistency.

--- Cla.py
+++ Cla.py
@@ -3,11 +3,49 @@ import Validity as V
 class Cla:
 
     def __init__(self):
-        self.tok = []
-        self.typ = []
+
+        self.tok = set()
+        self.typ = set()
+
+        self.validities = {}
 
         self.valid = V.HasType.VALID
         self.invalid = V.HasType.INVALID
 
-    def isValid(self):
-    	return self.invalid
+
+    def isValid(self, someToken, someType):
+
+        typeSet = self.getTypes(someToken)
+
+        if someType not in typeSet:
+            return self.invalid
+
+        else:
+            return self.valid
+
+
+    def addToken(self, newToken):
+        self.tok.add(newToken)
+
+
+    def addType(self, newType):
+        self.typ.add(newType)
+
+
+    def getTypes(self, someToken):
+
+        if someToken in self.validities:
+            return self.validities[someToken]
+
+        return set()
+
+
+    def addValidity(self, someToken, someType):
+
+        self.tok.add(someToken)
+        self.typ.add(someType)
+
+        if someToken not in self.validities:
+            self.validities[someToken] = set()
+
+        self.validities[someToken].add(someType)
--- test/Test_Cla.py
+++ test/Test_Cla.py
@@ -1,6 +1,6 @@
 import unittest
 
-from test_context import Cla
+from test_context import Cla as C
 from test_context import Validity as V
 
 
@@ -9,10 +9,41 @@ class Test_Cla(unittest.TestCase):
     valid = V.HasType.VALID
     invalid = V.HasType.INVALID
 
-    def test_isValid_returnsInvalidIfTokenDoesNotHaveType(self):
-        systemUnderTest = Cla.Cla()
 
-        self.assertEqual(self.invalid, systemUnderTest.isValid())
+    def test_isValid_returnsInvalidIfTokenDoesNotHaveType(self):
+        systemUnderTest = C.Cla()
+
+        result = systemUnderTest.isValid('x', None)
+
+        self.assertEqual(self.invalid, result)
+
+
+    def test_addToken_addsNewTokenToTokenSet(self):
+        systemUnderTest = C.Cla()
+
+        systemUnderTest.addToken('t')
+
+        self.assertTrue('t' in systemUnderTest.tok)
+
+
+    def test_addType_addsNewTypeToTypeSet(self):
+        systemUnderTest = C.Cla()
+
+        systemUnderTest.addType('t')
+
+        self.assertTrue('t' in systemUnderTest.typ)
+
+
+    def test_addValidity_causes_isValidToReturnTrue(self):
+
+        systemUnderTest = C.Cla()
+
+        systemUnderTest.addValidity('x', 'alpha')
+
+        result = systemUnderTest.isValid('x', 'alpha')
+
+        self.assertEqual(self.valid, result)
+
 
 if __name__ == '__main__':
     unittest.main()
