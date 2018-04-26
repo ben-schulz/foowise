25a4d5027bc8a84fcf3a668428bf3b2e61318eeb
ben-schulz <benjamin.john.schulz@gmail.com>
2018-04-26 15:55:03 -0500

    test(classifications): add test documenting assumptions on tokens, types. Add test class 'EquableTestClass' in 'Test_Cla.py', as demonstration of assumption that tokens and types must implement both '__eq__' and '__hash__'.

--- test/Test_Cla.py
+++ test/Test_Cla.py
@@ -9,7 +9,6 @@ class Test_Cla(unittest.TestCase):
     valid = V.HasType.VALID
     invalid = V.HasType.INVALID
 
-
     def test_isValid_returnsInvalidIfTokenDoesNotHaveType(self):
         systemUnderTest = C.Cla()
 
@@ -43,6 +42,47 @@ class Test_Cla(unittest.TestCase):
         result = systemUnderTest.isValid('x', 'alpha')
 
         self.assertEqual(self.valid, result)
+
+    def test_addValidity_independentOfImplementationTypes(self):
+
+        systemUnderTest = C.Cla()
+
+        testToken = FooClass(1)
+        testType = BarClass(2)
+
+        systemUnderTest.addValidity(testToken, testType)
+
+        result = systemUnderTest.isValid(testToken, testType)
+
+        self.assertEqual(self.valid, result)
+
+
+
+class EquableTestClass:
+
+    def __init__(self, primitiveValuedId):
+        self.id = primitiveValuedId
+
+    def __hash__(self):
+        return self.id
+
+    def __eq__(self, other):
+
+        try:
+            return self.id == other.id
+
+        except AttributeError:
+            return False
+
+
+    def __neq__(self, other):
+        return not self.eq(other)
+
+class FooClass(EquableTestClass):
+    pass
+
+class BarClass(EquableTestClass):
+    pass
 
 
 if __name__ == '__main__':
