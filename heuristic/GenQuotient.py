e93aedb07120acae80655449c56dcbc71adcd652
ben-schulz <benjamin.john.schulz@gmail.com>
2018-06-09 23:29:23 -0500

    feat(heuristic): generate a random quotient.
    Implement a generator that yields a random quotient of a given
    classification, so that these can be used to explore the space
    of possible channels.

--- heuristic/GenQuotient.py
+++ heuristic/GenQuotient.py
@@ -1,2 +1,12 @@
+import Set as S
+
 import Cla as C
 import Invariant as I
+
+
+def get_invariant(cla):
+
+    subset = S.Set.rand_subset(cla.typ)
+
+    while True:
+        yield I.Invariant(cla, next(subset))
--- test/Test_GenQuotient.py
+++ test/Test_GenQuotient.py
@@ -1,9 +1,26 @@
 import unittest
 
+import Cla as C
+import Invariant as I
 import GenQuotient as Q
 
-class Test_GenQuotient:
-    pass
+class Test_GenQuotient(unittest.TestCase):
+
+    def test_finds_an_invariant(self):
+
+        c = C.Cla({
+            'x': {1,2,3,4,5,6},
+            'y': {2,4,6},
+            'z': {3,6},
+            'u': {2,4},
+            'v': {1,3,5},
+        })
+
+        inv = next(Q.get_invariant(c))
+
+        quot = inv.quotient()
+
+        self.assertTrue(isinstance(inv, I.Invariant))
 
 
 if __name__ == '__main__':
