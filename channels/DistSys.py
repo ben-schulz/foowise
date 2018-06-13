061d86d2987feeea67227c35003aa5c66fe2333b
ben-schulz <benjamin.john.schulz@gmail.com>
2018-06-12 21:48:57 -0500

    feat(distsys): expose classifications.
    Make an iterable of classifications a member of 'DistSys',
    so that they can be directly retrieved, e.g. to calculate
    the colimit. Add method 'get_cla' to look up an indvidual
    classification by index. Add method 'get_infomorphisms',
    so that all infomorphisms of two classifications,
    specified by index, can also be used in constructions.

--- channels/DistSys.py
+++ channels/DistSys.py
@@ -7,4 +7,14 @@ class DistSys:
 
         clas = [i.proximal for i in infs] + [i.distal for i in infs]
 
-        self.clas = {i.index:i for i in clas}
+        self._clas = {c.index:c for c in clas}
+        self.clas = self._clas.values()
+
+    def get_cla(self, ix):
+        return self._clas.get(ix, None)
+
+    def get_infomorphisms(self, prox_ix, dist_ix):
+
+        return [inf for inf in self.infs
+                if inf.proximal.index == prox_ix
+                and inf.distal.index == dist_ix]
--- test/Test_DistSys.py
+++ test/Test_DistSys.py
@@ -8,9 +8,9 @@ class Test_DistSys(unittest.TestCase):
 
     def test_distinguishes_classifications_in_infomorphisms(self):
 
-        i = 0
-        j = 1
-        k = 2
+        i = 1
+        j = 2
+        k = 3
 
         c1 = C.Cla({'x':{0}}, index=i)
         c2 = C.Cla({'y':{1}}, index=j)
@@ -29,9 +29,9 @@ class Test_DistSys(unittest.TestCase):
 
         self.assertEqual(3, len(d.clas))
 
-        self.assertTrue(c1.index in d.clas)
-        self.assertTrue(c2.index in d.clas)
-        self.assertTrue(c3.index in d.clas)
+        self.assertEqual(c1.index, d.get_cla(c1.index).index)
+        self.assertEqual(c2.index, d.get_cla(c2.index).index)
+        self.assertEqual(c3.index, d.get_cla(c3.index).index)
 
 
     def test_classifications_not_duplicated_when_shared(self):
@@ -50,7 +50,39 @@ class Test_DistSys(unittest.TestCase):
         d = D.DistSys(infs)
 
         self.assertEqual(1, len(d.clas))
-        self.assertTrue(c.index in d.clas)
+        self.assertNotEqual(None, d.get_cla(c.index))
+
+
+    def test_get_infomorphisms_returns_list_of_all(self):
+
+        i = 1
+        j = 2
+        k = 3
+
+        c1 = C.Cla({'x':{0}}, index=i)
+        c2 = C.Cla({'y':{1}}, index=j)
+        c3 = C.Cla({'z':{2}}, index=k)
+
+        f_up_1 = lambda _: 1
+        f_down_1 = lambda _: 'x'
+        inf_1_2 = I.Infomorphism(c1, c2, f_up_1, f_down_1)
+
+        inf_2_1 = inf_1_2.dual
+
+        f_up_2 = lambda _: 2
+        f_down_2 = lambda _: 'y'
+        inf_2_3 = I.Infomorphism(c2, c3, f_up_2, f_down_2)
+
+        infs = [inf_1_2, inf_2_1, inf_2_3]
+        d = D.DistSys(infs)
+
+        c1_to_c2 = d.get_infomorphisms(c1.index, c2.index)
+        c2_to_c1 = d.get_infomorphisms(c2.index, c1.index)
+        c2_to_c3 = d.get_infomorphisms(c2.index, c3.index)
+
+        self.assertTrue(inf_1_2 in c1_to_c2)
+        self.assertTrue(inf_2_1 in c2_to_c1)
+        self.assertTrue(inf_2_3 in c2_to_c3)
 
 
 if __name__ == '__main__':
